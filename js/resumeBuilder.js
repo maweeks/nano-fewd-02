5fd051c2d0ff0c6bfdb099244647a8e6339f79b6
Matt Weeks <matt@weeks.codes>
2014-10-26 01:21:39 +0100

    Use bio variable.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,25 @@
-var name = "Matthew Weeks";
-var role = "Student";
+var bio = {
+	"name": "Matthew Weeks",
+	"role": "Student",
+	"contacts": {
+		"email": "m_weeks@hotmail.co.uk",
+		"github": "maweeks",
+		"location": "Canterbury, UK",
+		"mobile": "(+44) 7515 772 568",
+		"twitter": "@mad_mn"
+	},
+	"welcomeMessage": "Welcome to my resume, please contact me if you have any questions.",
+	"skills": [
+	  "Java", "JavaScript", "HTML", "CSS"
+	],
+	"bioPic": "http://nano.weeks.codes/img/logof-100.jpg"
+};
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+
+$("#main").append(bio.skills);
