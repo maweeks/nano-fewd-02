1001c3925a030490a7f309a01cbba41193534774
Matt Weeks <matt@weeks.codes>
2014-10-26 01:26:51 +0000

    Clean resumeBuilder.js and add the 4 main variables.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,25 +1,92 @@
 var bio = {
 	"name": "Matthew Weeks",
 	"role": "Student",
+	"welcomeMessage": "Welcome to my resume, please contact me if you have any questions.",
 	"contacts": {
+		"mobile": "(+44) 7515 772 568",
 		"email": "m_weeks@hotmail.co.uk",
 		"github": "maweeks",
-		"location": "Canterbury, UK",
-		"mobile": "(+44) 7515 772 568",
-		"twitter": "@mad_mn"
+		"twitter": "@mad_mn",
+		"location": "Canterbury, UK"
 	},
-	"welcomeMessage": "Welcome to my resume, please contact me if you have any questions.",
 	"skills": [
-	  "Java", "JavaScript", "HTML", "CSS"
-	],
-	"bioPic": "http://nano.weeks.codes/img/logof-100.jpg"
+	  "Java", "Visual Basic", "JavaScript", "HTML", "CSS"
+	]
 };
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var education = {
+	"schools": [
+        {
+        	"name": "Poole Grammar School",
+        	"location": "Poole, Dorset, UK",
+        	"degree": "A Levels",
+        	"majors": ["Chemistry", "Computer Science", "Maths"],
+        	"dates": "Sep 2005 - Jul 2011",
+        	"url": "http://www.poolegrammar.com/"
+        },
+        {
+        	"name": "University of Kent",
+        	"location": "Canterbury, Kent, UK",
+        	"degree": "BSc",
+        	"majors": ["Computer Science"],
+        	"dates": "Sep 2011 - Jun 2015",
+        	"url": "http://www.kent.ac.uk/"
+        }
+	],
+	"onlineCourses": [
+		{
+			"title": "Intro to HTML and CSS",
+			"school": "Udacity",
+			"dates": "Sep 2014",
+			"url": "https://www.udacity.com/course/ud304"
+		},
+		{
+			"title": "How to Use Git and GitHub",
+			"school": "Udacity",
+			"dates": "Oct 2014",
+			"url": "https://www.udacity.com/course/ud775"
+		},
+		{
+			"title": "JavaScript Basics",
+			"school": "Udacity",
+			"dates": "Oct 2014",
+			"url": "https://www.udacity.com/course/ud804"
+		}
+	]
+};
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+var projects = {
+    "projects": [
+        {
+            "title": "AVOS",
+            "dates": "Jan 2014 - May 2014",
+            "description": "A visualization to improve visibily for OpenStack.",
+            "images": []
+        },
+        {
+            "title": "Portfolio",
+            "dates": "Sep 2014",
+            "description": "A portfolio created using CSS and HTML to showcase my work.",
+            "images": []
+        },
+        {
+            "title": "Interactive Resume",
+            "dates": "Oct 2014",
+            "description": "A webpage to show my skills, education and past work.",
+            "images": []
+        }
+    ]
+};
 
+var work = {
+	"jobs": [
+		{
+			"employer": "Cisco Systems",
+			"title": "Cloud Developer",
+			"location": "San Jose, CA, USA",
+			"dates": "Aug 2013 - Jul 2014",
+			"description": "Working on the OpenSource project OpenStack, projects include creating a visualization tool to give greater visibility of the stack."
+		}
+	]
+};
 
-$("#main").append(bio.skills);
